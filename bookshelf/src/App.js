2a2be8ca8ffa1ecdd4fa8654d751b85ca48bbfd3
lukecarnevale <lukecarnevale19@gmail.com>
2020-06-20 19:36:43 -0400

    bootstrap - still having the same issues with toggle

--- bookshelf/src/App.js
+++ bookshelf/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import books from './books.json'
-import 'bootstrap/dist/css/bootstrap.css'
+import { Container, Row, Col, UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
 
 class App extends React.Component {
   constructor (props) {
@@ -22,17 +22,30 @@ class App extends React.Component {
         <h1>Welcome to Freeshelf!</h1>
         {books.map((bookDetail, index) => {
           return <div key={index}>
-            <h1>{bookDetail.title}</h1>
-            <img src={bookDetail.coverImageUrl} width='25%' />
-            <p>{bookDetail.author}</p>
-            <p>{bookDetail.shortDescription}</p>
-            <p>{this.state.isToggleOn && bookDetail.coverImageUrl}</p>
-            <p>{this.state.isToggleOn && bookDetail.publisher}</p>
-            <p>{this.state.isToggleOn && bookDetail.publicationDate}</p>
-            <p>{this.state.isToggleOn && bookDetail.detailedDescription}</p>
-            <button className='button'onClick={this.handleClick}>
-              {this.state.isToggleOn ? 'Read Less' : 'Read More'}
-            </button>
+            <h2 className="title">{bookDetail.title}</h2>
+            <div className="d-flex bd-highlight example-parent">
+              <div className="p-2 w-100 bd-highlight col-example">
+              <p>Author: {bookDetail.author}</p>
+              <p>{bookDetail.shortDescription}</p> </div>
+              <div className="p-2 flex-shrink-1 bd-highlight col-example">
+              <img src={bookDetail.coverImageUrl} width='25%' />
+              </div>
+            </div>
+            <div>
+              <Button color="primary" id="toggler" style={{ marginBottom: '1rem' }}>
+                Read More
+              </Button>
+              <UncontrolledCollapse toggler="#toggler">
+                <Card>
+                  <CardBody>
+                  <p><em>Url: </em>{bookDetail.coverImageUrl}</p>
+                      <p><em>Publisher: </em>{bookDetail.publisher}</p>
+                      <p><em>Publication Date: </em>{bookDetail.publicationDate}</p>
+                      <p><em>Description: </em>{bookDetail.detailedDescription}</p>
+                  </CardBody>
+                </Card>
+              </UncontrolledCollapse>
+            </div>
           </div>
         })}
       </div>
